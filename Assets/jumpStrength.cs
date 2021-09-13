c1000a7260a3a421e1a7cf912c3ad1e3b54cdf17
xakzi <emanoel.kouriat@student.forsbergsskola.se>
2021-09-13 16:58:31 +0200

    Updated Step 3
    Added Movement and jumping for the player

---
+++ Assets/jumpStrength.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jumpStrength : MonoBehaviour
+{
+    public float upForce = 15000f;
+
+    private Rigidbody rb;
+    bool canJump = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("space") && canJump == true)
+        {
+            rb.velocity = Vector3.zero;
+            rb.AddForce(new Vector3(0, upForce));
+            canJump = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            canJump = true;
+        }
+    }
+}
